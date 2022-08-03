9c4b5faf3f5c88ab7bea3716696784b0d47e8588
iverty <berezhnoy20025@gmail.com>
2022-08-03 19:07:59 +0300

    create question AC spec

--- spec/acceptance/questions/create_question_spec.rb
+++ spec/acceptance/questions/create_question_spec.rb
@@ -8,6 +8,26 @@ feature 'Create question', '
 
   given(:user) { create(:user) }
 
+  context 'multiple sessions' do
+    scenario 'question appears on another user\'s page', js: true do
+      Capybara.using_session('guest') do
+        visit questions_path
+      end
+
+      Capybara.using_session('user') do
+        sign_in(user)
+        create_question
+        expect(page).to have_content 'Your question was created successfully'
+        expect(page).to have_content 'Test question'
+        expect(page).to have_content 'Test body'
+      end
+
+      Capybara.using_session('guest') do
+        expect(page).to have_link 'Test question'
+      end
+    end
+  end
+
   scenario 'Authenticated user creates question' do
     sign_in(user)
     create_question
