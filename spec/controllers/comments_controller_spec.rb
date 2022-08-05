e538143fcc474bf7a9053d86eee32cbb82f8a6f0
iverty <berezhnoy20025@gmail.com>
2022-08-05 15:09:13 +0300

    specs for comments controller

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe CommentsController, type: :controller do
+  describe 'POST comments#create' do
+    context 'with valid attrs' do
+      before do
+        # devise stuff
+        allow(controller).to receive(:authenticate_user!).and_return(true)
+        allow(controller).to receive(:current_user).and_return(create(:user))
+      end
 
+      it 'saves new comment to db' do
+        expect { post :create, params: { comment: attributes_for(:comment) } }.to change(Comment, :count).by(1)
+      end
+    end
+
+    context 'with invalid attrs' do
+      it 'not saved new comment to db' do
+        expect { post :create, params: { comment: attributes_for(:comment) } }.to_not change(Comment, :count)
+      end
+    end
+  end
 end
--- spec/factories/comments.rb
+++ spec/factories/comments.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :comment do
-    
+    text { 'Test comment text' }
+    commentable_type { 'Answer' }
+    commentable_id { create(:answer).id }
   end
 end
