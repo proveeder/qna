f5e60ae1699c573f96400f82bca9103217d5d81a
iverty <berezhnoy20025@gmail.com>
2022-08-11 19:55:47 +0300

    lesson done

--- spec/api/v1/profiles_spec.rb
+++ spec/api/v1/profiles_spec.rb
@@ -16,12 +16,24 @@ describe 'Profile API' do
 
     context 'authorized' do
       let(:me) { create :user }
-      let(:token) { create(:access_token) }
+      let(:token) { create(:access_token, resource_owner_id: me.id) }
 
-      before { get '/api/v1/profiles/me', headers: { 'Authorization': "Bearer #{token.token}" } }
+      before { get '/api/v1/profiles/me', headers: { 'Authorization': "Bearer #{token.token}" }, as: :json }
 
       it 'returns 200 status code' do
         expect(response.status).to eq 200
+      end
+
+      %w[id email created_at updated_at admin].each do |attr|
+        it "contains #{attr}" do
+          expect(response.body).to be_json_eql(me.send(attr.to_sym).to_json).at_path(attr)
+        end
+      end
+
+      %w[password encrypted_password].each do |attr|
+        it "contains #{attr}" do
+          expect(response.body).to_not have_json_path attr
+        end
       end
     end
   end
