f9a521ef08983d5df0ae1ff9da45e5d48291685a
iverty <berezhnoy20025@gmail.com>
2022-08-17 18:21:40 +0300

    delete folder after file being deleted

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -2,4 +2,13 @@ class Attachment < ApplicationRecord
   belongs_to :attachable, polymorphic: true
 
   mount_uploader :file, FileUploader
+
+  after_destroy :delete_file_directory
+
+  private
+
+  # delete directory where file was uploaded so no trash remain after object been destroyed
+  def delete_file_directory
+    FileUtils.remove_dir("#{Rails.root}/public/uploads/#{self.class.to_s.underscore}/#{id}")
+  end
 end
--- spec/models/attachment_spec.rb
+++ spec/models/attachment_spec.rb
@@ -2,4 +2,13 @@ require 'rails_helper'
 
 RSpec.describe Attachment, type: :model do
   it { should belong_to :attachable }
+
+  describe '.delete_file_directory' do
+    let(:attachment) { create(:attachment) }
+
+    it 'deletes file directory' do
+      expect(attachment).to receive(:delete_file_directory)
+      attachment.destroy!
+    end
+  end
 end
