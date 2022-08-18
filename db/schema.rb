7fc796be394ddd64c556b1400fb5b854ef22e122
iverty <berezhnoy20025@gmail.com>
2022-08-18 12:42:29 +0300

    reinstalled

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_16_190155) do
+ActiveRecord::Schema.define(version: 2022_08_16_105519) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -105,10 +105,6 @@ ActiveRecord::Schema.define(version: 2022_08_16_190155) do
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id"
     t.integer "best_answer_id"
-    t.bigint "question_id"
-    t.bigint "question_id_id"
-    t.index ["question_id"], name: "index_questions_on_question_id"
-    t.index ["question_id_id"], name: "index_questions_on_question_id_id"
     t.index ["user_id"], name: "index_questions_on_user_id"
   end
 
