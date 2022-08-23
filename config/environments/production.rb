a0be1248c8a673ef99b5b64b05a645138900ecc6
alex <berezhnoy20025@gmail.com>
2022-08-23 15:25:26 +0300

    smtp

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -124,11 +124,11 @@ Rails.application.configure do
 
   # SMTP settings for gmail
   config.action_mailer.smtp_settings = {
-    :address => "smtp.gmail.com",
-    :port => 587,
-    :user_name => 'ababagalamagz@gmail.com',
-    :password => 'xxkkyuzxuadapvrv',
-    :authentication => "plain",
-    :enable_starttls_auto => true
+    address: "smtp.gmail.com",
+    port: 587,
+    user_name: 'ababagalamagz@gmail.com',
+    password: 'xxkkyuzxuadapvrv',
+    authentication: "plain",
+    enable_starttls_auto: true
   }
 end
